ef17af1ededa5314086f937064084ac56e238425
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-21 01:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    var tb = document.getElementById('sampleTable');
+    var gt =  tb.innerHTML;
+    tb.innerHTML = "<tr><td>New cell1</td><td>New cell2</td></tr>" + gt;
 }
