7eb20ec37baf8757666b74113eb3c975c53ebfff
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-04-28 16:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function insert_Row() {
     var tb = document.getElementById('sampleTable');
-    var gt =  tb.innerHTML;
-    tb.innerHTML = "<tr><td>New Cell1</td><td>New Cell2</td></tr>" + gt;
+    var gt = tb.innerHTML;
+    
+    var cr = document.createElement('tr');
+    var cr2 = document.createElement('tr');
+
+    var cell1 = document.createElement('td');
+    cell1.innerHTML = 'New Cell1';
+
+    var cell2 = document.createElement('td');
+    cell2.innerHTML = 'New Cell2';
+
+    cr.appendChild(cell1);
+    cr2.appendChild(cell2);
+
+    tb.insertBefore(cr2, tb.firstChild);
+    tb.insertBefore(cr, tb.firstChild);
 }
