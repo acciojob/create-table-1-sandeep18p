44b568f1299e1b07d92917c276d0fab5d52b88ba
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-04-28 16:15:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function insert_Row() {
     var tb = document.getElementById('sampleTable');
     var gt =  tb.innerHTML;
-    var cr =  documtnet.createElement('tr');
-	cr.innerHtml = <td>New Cell1</td>
-	 var cr2 =  documtnet.createElement('tr');
-	cr2.innerHtml = <td>New Cell2</td>
     tb.innerHTML = "<tr><td>New Cell1</td><td>New Cell2</td></tr>" + gt;
 }
